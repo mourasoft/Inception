da581c7cac531771148ede3ec5fa3851cb6a883d
Mourad Meski <mmeski@e2r3p5.1337.ma>
2022-05-01 18:21:20 +0000

    install adminer

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,18 +6,15 @@ else
     echo "wordpress does not exist"
     cp -r /tmp/wordpress /var/www/html/
     cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-    sed -i 's/database_name_here/inception/g' /var/www/html/wordpress/wp-config.php
-    sed -i 's/username_here/mouras/g' /var/www/html/wordpress/wp-config.php
-    sed -i 's/password_here/mouras/g' /var/www/html/wordpress/wp-config.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wordpress/wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/" /var/www/html/wordpress/wp-config.php
     sed -i 's/localhost/mariadb/g' /var/www/html/wordpress/wp-config.php
-
     chown -R www-data:www-data /var/www/html/wordpress
-    # wp config create --dbname=inception --dbuser=mouras --dbpass=mouras --dbhost=mariadb --allow-root --path=/var/www/html/wordpress
-    # wp  core config --dbname=inception --dbuser=mouras --dbpass=mouras --path=/var/www/html/wordpress --allow-root 
     sleep 5
-    wp core install --url=https://localhost --title='inception' --admin_user=ashley --admin_email=hello@ashleyrich.com --admin_password=password --path=/var/www/html/wordpress --allow-root 
-
-
+    wp core install --url=https://localhost --title='inception' --admin_user=$WP_ADMIN_USER --admin_email=hello@ashleyrich.com --admin_password=$WP_ADMIN_USER_PASSWORD --path=/var/www/html/wordpress --allow-root 
+    wp user create $WP_USER mouras@gmail.com --role=subscriber  --user_pass=$WP_USER_PASSWORD  --path=/var/www/html/wordpress  --allow-root 
+    wp theme install galaxis --activate --path=/var/www/html/wordpress --allow-root
 fi
 
 exec "$@"
